925dc779873ef0c5fad88445b3da5b7a588a843b
yujiOnuki <yujiuniuni@gmail.com>
2024-01-07 10:17:01 +0000

    コントローラー記述

---
+++ test/controllers/public/user_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class Public::UserControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get public_user_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get public_user_edit_url
+    assert_response :success
+  end
+
+  test "should get confirm" do
+    get public_user_confirm_url
+    assert_response :success
+  end
+end
