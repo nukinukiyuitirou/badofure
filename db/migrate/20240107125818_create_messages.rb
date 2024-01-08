d9ba390fc2c0b167bd9d0563b2f25f13a9cbca1c
yujiOnuki <yujiuniuni@gmail.com>
2024-01-08 13:31:53 +0000

    ルーティング大体完成

--- db/migrate/20240107125818_create_messages.rb
+++ db/migrate/20240107125818_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-
+      t.integer :user_id, null: false
+      t.integer :room_id, null: false
+      t.text :body, null: false
       t.timestamps
     end
   end
