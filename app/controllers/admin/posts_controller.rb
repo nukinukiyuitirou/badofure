8b06d931033bae39f5611db331617afc6caf7b69
yujiOnuki <yujiuniuni@gmail.com>
2024-01-16 14:04:36 +0000

    検索機能両方OK

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,6 +2,16 @@ class Admin::PostsController < ApplicationController
  before_action :authenticate_admin!, except: [:top]
   def index
     @posts = Post.all
+    if params[:region_id].present? && params[:region_id] != "1"
+      @posts = Region.find(params[:region_id]).posts
+    end
+    if params[:is_sex].present?
+      user_ids = User.where(is_sex: params[:is_sex]).pluck(:id)
+      @posts = @posts.where(user_id: user_ids)
+    end
+    if params[:level].present?
+      @posts = @posts.where(level: params[:level])
+    end
   end
 
   def show
