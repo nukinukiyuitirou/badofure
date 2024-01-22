aebf2f5260876530b5f0b98639363769c1410e75
yujiOnuki <yujiuniuni@gmail.com>
2024-01-22 12:49:49 +0000

    非同期修正

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,16 +1,16 @@
 class Admin::PostsController < ApplicationController
  before_action :authenticate_admin!, except: [:top]
   def index
-    @posts = Post.all.page(params[:page]).per(10)
+    @posts = Post.all.page(params[:page]).per(9).order(created_at: :desc)
     if params[:region_id].present? && params[:region_id] != "1"
-      @posts = Region.find(params[:region_id]).posts
+      @posts = Region.find(params[:region_id]).posts.page(params[:page]).per(9).order(created_at: :desc)
     end
     if params[:is_sex].present?
       user_ids = User.where(is_sex: params[:is_sex]).pluck(:id)
-      @posts = @posts.where(user_id: user_ids)
+      @posts = @posts.where(user_id: user_ids).page(params[:page]).per(9).order(created_at: :desc)
     end
     if params[:level].present?
-      @posts = @posts.where(level: params[:level])
+      @posts = @posts.where(level: params[:level]).page(params[:page]).per(9).order(created_at: :desc)
     end
   end
 
