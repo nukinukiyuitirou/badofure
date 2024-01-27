36d57468bb3ba2969a2158332593c197618187f1
yujiOnuki <yujiuniuni@gmail.com>
2024-01-27 04:24:39 +0000

    通知機能実装

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,10 +1,16 @@
 class Public::MessagesController < ApplicationController
 before_action :authenticate_user!
   def create
-    message = Message.new(message_params)
-    message.user_id = current_user.id
-    if message.save
-      redirect_to room_path(message.room)
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    if @message.save
+
+      # 新規作成された@messageに紐づくroomを@roomに格納する
+      @room = @message.room
+      # 本引数を２つ持たせてcreate_notification_dmメソッドを実行
+      @room.create_notification_dm(current_user, @message.id)
+
+      redirect_to room_path(@message.room)
     else
       redirect_back(fallback_location: root_path)
     end
